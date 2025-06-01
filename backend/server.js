e90562ce43feef01c42e1583d94bedc6299e37a8
TusharSingh04 <tusharsingh00769@gmail.com>
2025-06-01 19:27:35 +0530

    mongoose code error

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
 .then(() => {
   console.log('MongoDB connected successfully');
